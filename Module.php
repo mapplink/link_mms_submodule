559f55bb319827aebf833a240c4b386fdc84d356
gelysis <andreas@gelysis.net>
2016-12-12 15:57:08 +1300

    Initial commit with .gitignore and Module.php file

---
+++ Module.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category Mms
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2016 HealthPost Ltd.
+ * @license  Commercial - All Rights Reserved
+ */
+
+namespace Mms;
+
+use Application\MagelinkModule;
+
+
+class Module extends MagelinkModule {}
