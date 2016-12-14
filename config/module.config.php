4a9173b733fd771029eef7e0020fdbf2bf120539
gelysis <andreas@gelysis.net>
2016-12-15 10:07:26 +1300

    Added config files

---
+++ config/module.config.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @category Mms
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2016 LERO9 Ltd.
+ * @license Commercial - All Rights Reserved
+ */
+
+$moduleConfig = array (
+    'node_types'=>array(
+        'mms'=>array(
+            'module'=>'Mms',
+            'name'=>'Mms',
+            'entity_type_support'=>array(
+//                'product',
+                'order',
+                'stockitem'
+            ),
+            'config'=>array( // Config options to be displayed to the administrator
+                'web_url'=>array(
+                    'label'=>'MMS Base API URL',
+                    'type'=>'Text',
+                    'required'=>TRUE
+                ),
+                'app_id'=>array(
+                    'label'=>'Application ID',
+                    'type'=>'Text',
+                    'required'=>TRUE
+                ),
+                'app_key'=>array(
+                    'label'=>'Application Key',
+                    'type'=>'Text',
+                    'required'=>TRUE
+                ),
+                'marketplace_id'=>array(
+                    'label'=>'Marketplace ID',
+                    'type'=>'Text',
+                    'required'=>TRUE
+                )
+            )
+        )
+    ),
+    'service_manager'=>array(
+        'invokables'=>array(
+            'mms_rest'=>'Mms\Api\RestV1',
+            'mmsService'=>'Mms\Service\MmsService',
+            'mmsConfigService'=>'Mms\Service\MmsConfigService',
+        ),
+        'shared'=>array(
+            'mms_rest'=>FALSE,
+            'mmsService'=>FALSE,
+            'mmsConfigService'=>FALSE
+        )
+    )
+);
+
+return $moduleConfig;
