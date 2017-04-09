ec41e229dc194977db7e2d0563919d184c251692
gelysis <andreas@gelysis.net>
2017-04-10 00:12:21 +1200

    Added and changed license

--- src/Mms/Node.php
+++ src/Mms/Node.php
@@ -4,7 +4,7 @@
  * @category Mms
  * @author Andreas Gerhards <andreas@lero9.co.nz>
  * @copyright Copyright (c) 2016 HealthPost Ltd.
- * @license  Commercial - All Rights Reserved
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause - Please view LICENSE.md for more information
  */
 
 namespace Mms;
